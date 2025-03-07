507775c047e37745b01ed036e05372b54cd7e714
Chico-WebDeb <Chicowebdev@gmail.com>
2025-03-07 05:01:33 +0100

    My First Project Chatbot. First Version 1.0

---
+++ README.md
@@ -0,0 +1,60 @@
+--> Mon prémier project💼 de chatbot👾 en mettant en avant mon apprentissage💻 en js pour mieux tester mes connaissances avec fecth📌 ou import🎈 sur les API🚀🌍
+
+
+--->> l'API est spécifique à un domaine par exemple ici le sport, cela à été fais en JavaScript avec un filtrage ('inculde') 
+
+
+--->> La fonction asynchrone qui gère la vérification de la requette de l'utilisateur et la reponse en JSON.parse de l'API en prenant compte de la
+
+spécifité à la condition, puis la fonction marked qui parse la reponse de l'IA en y ajoutant le styles les textes gras et italique grace au script "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"
+
+
+--->> La fonction messageUser permmet de crée le contenant et recoit le contenu qui est la question de l'utilisateur et l'affiche dans le chat
+
+
+--->> La fonction messageIA permet de crée le contenant et recoit le contenu qui est la réponse de l'IA et l'affiche dans le chat
+
+
+--->> (02) Deux button permettant d'effectuer la fonction asynchrone qui fais tout le travail
+
+    ---->> la constante btn recoit le button qui pour contenu "Envoyer" et lorsqu'il est cliquer il excécute la fonction asynchrone
+
+   ---->> Un deuxieme addEventListener ajouter pour le keydown => event.key === "Enter" (qui signifie lorsque l'utilisateur tape Enter au clavier) alors s'excécute la fonction asynchrone
+
+
+--->> Le window addEventListener "DOMContentLoaded" qui permet de sauvergarder la conversation grâce au LocalStorage qui recoit la cle des valeurs sauvegarder, puis effectue la condition lorsque l'utilisateur fais la requette on appelle la fonction messageUser sinon lorsque l'IA envoie la reponse on appelle la fonction messageIA. 
+
+--->> on affiche le tableau du localstorage stocker dans le dom en console .
+
+
+********************* 1- LES DIFFICULTES DU PROJETS *********************
+
+------->> RECHERCHE DE L'API <<-------
+
+------->> INSERTION DE MON API DANS MON CODE <<-------
+
+------->> SAUVEGARDE DES DONNEES DE L'UTILISATEUR ET DE L'API DANS LE DOM AVEC LOCALSTORAGE <<-------
+
+------->> SPECIFIIER LE CHATBOT AU SPORT <<-------
+
+
+********************* 2- LES SOLUTIONS DU PROJETS *********************
+
+------->> L'API DE GOOGLE AI STUDIO <GEMINI API> <<-------
+
+------->> INSTALLATION DE L'API AVEC NODE.JS DANS L'INVITE DE COMMANDE <<"npm install @google/generative-ai">> <<-------
+
+------->> EXPLICATION COMPREHENSION ET RESOLUTION DU LOCALSTORAGE AVEC CHATGPT GEMINI AI ET GEEKSFORGEEKS/TUTORIEL-JAVASCRIPT SUR GeeksForGeeks.org <<-------
+
+------->> FILTRAGE DU PROMPT EFFECTUER DE LA RESPONSE DE L'IA AVEC INCLUDE, EN AJOUTER A LA VARIABLE PROMPT LA CONDITION SPECIFIQUE AU SPORT ET LA QUESTION DE L'UTILISATEUR <<-------
+
+********************* 3- TECHNOLOGIES UTILISES *********************
+
+-------->> <HTML> <<-------
+
+------->> <CSS> <<-------
+
+------->> <JAVASCRIPT> <<-------
+
+------->> <API> <<-------
+
--- chatBot.js
+++ chatBot.js
@@ -1,124 +1,165 @@
 
-import { GoogleGenerativeAI } from 'https://esm.sh/@google/generative-ai';
+import { GoogleGenerativeAI } from 'https://esm.sh/@google/generative-ai'; // Import de l'url et sa fonctionnalité depuis mon CMD (invite de commande)
 
-// MY API KEY
-const genAI = new GoogleGenerativeAI("AIzaSyDSIz_r2cWORPIN_5ShytJlfJyW_td6tmY");
+const genAI = new GoogleGenerativeAI("AIzaSyDSIz_r2cWORPIN_5ShytJlfJyW_td6tmY");  // MY API KEY
 
-// MY API MODEL GENERATIVE
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: "You are a chatbot. Your name is CHICO JD.", });  // MY API MODEL GENERATIVE
 
-// la div qui affiche les chat entre l'utilisateur et l'APi
-const chatMessage = document.getElementById("chat-text")
+const chatMessage = document.getElementById("chat-text");  // la div qui affiche les chat entre l'utilisateur et l'APi
 
-// la zone de saisie de l'utilisteur
-const inputText = document.getElementById("input-text");
+const inputText = document.getElementById("input-text"); // la zone de saisie de l'utilisteur
 
-// le button qui permet d'envoyer la requette
-const btn = document.getElementById("btn");
+const btn = document.getElementById("btn");  // le button qui permet d'envoyer la requette
 
-//la span qui montre le chargement de la requette
-const loading = document.getElementById('loading')
+const loading = document.getElementById('loading');  //la span qui montre le chargement de la requette
 
 
-// ma fontion asynchrone affiche la question et la reponse de l'utilisateur dans le contenu chatMessage
-async function maFonctionAsynchrone() {
-   
-   // permet de gerer les erreurs survenu 
-   // try gère le cas ou il ya pas d'erreur
-   try {
-      // recupère la valeur de mon input dont la requette saisie par user dans ma constante prompt
-      let prompt = inputText.value.trim();
 
-      // une condition si le prompt contient une valeur alors exécute la requette 
-      if(prompt) {
-
-         //la fonction messageUser permet de creer une div et d'ajouter le prompt à l'intérieur
-         messageUser(prompt);
-
-         // après que la requette soit faite et en attente du chargement de la reponse on affiche un message de cgargement des données
-         chatMessage.appendChild(loading).textContent = 'Un instant...'
-      
-         // la constante result recois une attente de la lecture du prompt de l'utilisateur par le model de l'API utiliser
-         const result = await model.generateContent(prompt);
-
-         //la constante response attend la reponse de l'IA sur la requette
-         const response = await result.response.text();
-
-         // puis la fonction messageCHICO cree une div et parse avec marked grace au cdn js dans le head le text avec des text gras et italique de la reponse de l'IA puis affiche la reponse
-         messageCHICO(marked.parse(response));
-   // on fais disparaitre le chargement de la reponse
-   loading.textContent = '';
-
-   //on efface la valeur entré par user lorsque la requette est resolu
-   inputText.value = "";
+function saveToLocalStorage(messages) {
+   localStorage.setItem("chatHistory", JSON.stringify(messages));  // j'enrégistre chaque message de l'utilisateur et celui de l'ia dans chatHistory a partir de cette fonction.
 }
 
+
+let chatHistory = JSON.parse(localStorage.getItem("chatHistory")) || []; // Tableau global pour stocker l'historique des messages
+
+
+
+async function maFonctionAsynchrone() {  // ma fontion asynchrone  véreifie la spécialité avec prompt et l'excécute avec response puis fais appel au fonction de l'affichage des requettes du user et la reponse de lIA
+
+
+   // permet de gerer les erreurs survenu 
    
-//catch gère le cas oui il y'a erreur
-   } catch (error) {
+   try {  // try gère le cas ou il ya pas d'erreur
 
-      //affiche l'erreur dans la console
-console.log("Erreur :", error);
-
-// affiche le texte dans le dom
-chatMessage.innerText = "Une erreur est survenu"
-   }
-   }
-
-   // cette fonction cree la div pour afficher la saisie du user 
-   function messageUser(message) {
       
-      //creation de la div dans messageElement
-      const messageElement = document.createElement('div');
+      let question = inputText.value.trim();  // recupère la valeur de mon input dont la requette saisie par user dans ma constante prompt
 
-      //afichage du contenu dans la div avec pour paramètre message
-      messageElement.textContent = "vous : " + message;
+      // prompt recoit sa specificité et la saisie du user de sorte que l'ia recoit la specifité comme requette accomppagné de la question et repond en fois de la condition
+      let prompt = `Réponds uniquement aux questions liées au sport. Si la question n'est pas liée au sport, réponds simplement "Désolé, je réponds uniquement aux questions liées au sport."${question}`;
 
-      //style appliqué a cette div
-      messageElement.style.backgroundColor = '#cccccc42';
-      messageElement.style.float = 'right'
-      messageElement.style.borderRadius = '10px'
-      messageElement.style.padding = '10px'
-      messageElement.style.width = '50%'
+      if(prompt) {  // une condition si le prompt contient notre condition et la question alors exécute la requette 
+         
+         const result = await model.generateContent(prompt);   // la constante result recois une attente de la lecture du prompt de l'utilisateur par le model de l'API utiliser
 
-      //afficher la div en tant que element enfant dans son élément parent chatMessage
-      chatMessage.appendChild(messageElement);
+         const response = await result.response.text();           //la constante response attend la reponse de l'IA sur la requette du user
 
-      //ajouter un effet scroll
-      chatMessage.scrollTop = chatMessage.scrollHeight;
+         if(response.includes("Désolé, je réponds uniquement aux questions liées au sport.")) {  // Filtrer la reponse pour le sport lorsqu'il contient  notre condition etablie dans le prompt et informer le user sur la condition de la spécialité pour le sport
+            
+
+            let mauvaiseRequette = "Désolé je ne suis pas en capacité de répondre a cette question je suis une IA spécifié sur le sport .! Alors que voulez vous savoir sur le sport ?" // on affecte ce message a une variable pour faire appel a la fonction de l'IA(messageIA) l'afficher au dom comme reponse de l'IA
+            
+            messageUser(question); // 1- La fonction recoit pour paramètre la question du user 
+            inputText.value = "" // 2- on vide notre formule de saisie apres la fonction effectuée
+            chatHistory.push({role: "user", message: question}); // 3 -on pousse la saisie ou les données du user dans le tableau
+            saveToLocalStorage(chatHistory); // 4- on sauvegarde les valeurs du tableau grace a cette fonction 
+
+            // 5- après que la requette soit faite et en attente du chargement de la reponse on affiche un message de chargement des données
+            chatMessage.appendChild(loading).textContent = 'Un instant...'            
+      
+
+            // 6- Ajout d'un setTime pour mettre 1.5s pour afficher la reponse dans l'IA et permettre d'afficher notre "un instant"
+                 setTimeout(() => {
+                  
+                     messageIA(mauvaiseRequette);  // 7- La fonction recoit pour paramètre la question de L'IA
+                     chatHistory.push({role: "ia", message: mauvaiseRequette})  // 8- on pousse la saisie ou les données de l'IA dans le tableau
+                     saveToLocalStorage(chatHistory)                              // 9- on sauvegarde les valeurs du tableau grace a cette fonction 
+                     chatMessage.appendChild(loading).textContent = '';  // 10- on fais disparaitre le message "un instant" qui permet de signifier le chargement des données
+                     
+
+                 }, 1500);
+            
+         } else {    // sinon lorsque la requette du user est lié au sport l'IA repond correctement 
+        
+               
+               if(question === "") {            // si le user laisse le formulaire vide et appuie entrer alors
+                  return alert("Veuiller Entrer une demande");  // il recoit cette alerte
+               }
+            
+                  messageUser(question);  // 1
+                  inputText.value = "";  // 2
+                  chatHistory.push({role: "user", message: question}); // 3
+                  saveToLocalStorage(chatHistory);  // 4
+                  chatMessage.appendChild(loading).innerText = "Un instant...";  // 5
+       
+            
+                  // 6
+                  setTimeout(() => {
+
+                     messageIA(marked.parse(response)); // 7
+                     chatHistory.push({role: "ia", message: marked.parse(response)});  // 8
+                     saveToLocalStorage(chatHistory);  // 9
+                     chatMessage.appendChild(loading).innerText = "";  // 10
+                     
+                  }, 1500);
+                                       
+               }
+
+      } 
+   
+
+   } catch (error) {  //catch gère le cas oui il y'a erreur
+
+         console.log("Erreur :", error);   //affiche l'erreur dans la console
+
+         chatMessage.innerText = "Une erreur est survenu"  // affiche le texte dans le dom
+
    }
+}
+
+  
+function messageUser(message) {     // cette fonction cree la div pour afficher la saisie du user 
+      
+      const messageElement = document.createElement('div');        //creation de la div dans messageElement
+
+      messageElement.textContent = message;       //afichage du contenu dans la div avec pour paramètre message
+
+      messageElement.classList.add("messageElementUser")  //style appliqué a cette div
+      
+      chatMessage.appendChild(messageElement);   //afficher la div en tant que element enfant dans son élément parent chatMessage
+
+      chatMessage.scrollTop = chatMessage.scrollHeight;  //ajouter un effet scroll
+
+}
 
 
-   //la fonction messageCHICO cree une div qui contient la reponse de l'API
-   function messageCHICO(message) {
 
-      // creation de la div dans messageElement
-      const messageElement = document.createElement('div');
+function messageIA(message) {     // la fonction messageCHICO cree une div qui contient la reponse de l'API et l'affiche avec pour paramètre message
+  
+      const messageElement = document.createElement('div');    // creation de la div dans messageElement
 
-      //afiche le contenu dans la div dont la reponse de l'API avec comme paramètre message
-      messageElement.innerHTML = "CHICO JD : " + message;
+      messageElement.innerHTML = message;  //afiche le contenu dans la div dont la reponse de l'API 
 
-      //style appliqué a la div
-      messageElement.style.marginBottom = '30px'
-      messageElement.style.lineHeight = '40px'
+      messageElement.classList.add("messageElementCHICO")  //ajout d'une classe pour styliser mon message de la requette
 
-      //afficher la div en tant que element enfant dans son élément parent chatMessage
-      chatMessage.appendChild(messageElement);
+      chatMessage.appendChild(messageElement);        //afficher la div en tant que element enfant dans son élément parent chatMessage
 
-      //ajoute un effet scroll
-      chatMessage.scrollTop = chatMessage.scrollHeight
-   }
+      chatMessage.scrollTop = chatMessage.scrollHeight   //ajoute un effet scroll au contenu du chat pour déscendre et monter
+      
+}
 
 
-// Ajoute la fonction maFonctionAsynchrone lorsqu'on sur le button qui pour text envoyer
-btn.addEventListener('click', maFonctionAsynchrone);
 
-// losqu'on tape Entrer au clavier il effectue aussi la fonction maFonctionAsynchrone
-inputText.addEventListener('keydown', function(event) {
+
+btn.addEventListener('click', maFonctionAsynchrone);  // Ajoute la fonction maFonctionAsynchrone lorsqu'on clique sur le button  envoyer
+
+   inputText.addEventListener('keydown', function(event) {  // losqu'on tape Entrer au clavier il effectue aussi la fonction maFonctionAsynchrone
    if(event.key === 'Enter') {
       maFonctionAsynchrone();
    }
 })
 
 
-  
+window.addEventListener("DOMContentLoaded", () => {  // stocker l'historique dans le tableau chatHistory et l'afficher dans le domaine après réchargement de la page
+   let chatHistory = JSON.parse(localStorage.getItem("chatHistory")) || [];
+
+   chatHistory.forEach(({ role, message }) => {     //chatHistory est maintenant un tableau donc peut parcourir grace a forEach les données envoyés par l'utilisateur("user") et l'IA("IA")
+
+       if (role === "user") {  //si le role(donnée) est celui envoyé par l'utilisateur("user") on appel la function qui crée le contenant et affiche les données à l'intérieur
+           messageUser(message);
+       } else {   // sinon on fais pareil avec les données qui sont envoyé par l'IA et on appelle sa fonction
+           messageIA(message);
+       }
+   });
+
+   console.log("chargement des données : .....", chatHistory)   // affiche le tableau de l'historique en console pour le suivie de tout fonctione
+});
